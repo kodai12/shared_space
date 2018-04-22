c734e6bdc402714f14b8fe1e05cd64e567570557
sakochi_koudai <sakochi_koudai@aratana.jp>
2018-04-22 16:08:25 +0900

    fix: FileList

--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -4,24 +4,28 @@
     <img class="preview-img" src="" alt="">
     <h2 class="title">sample</h2>
     <span class="created-at">2018/01/01 00:00:00</span>
+    <span class="author">sakochi</span>
     <a class="img-link" href="#">link</a>
   </li>
   <li class="file-list__item">
     <img class="preview-img" src="" alt="">
     <h2 class="title">sample</h2>
     <span class="created-at">2018/01/01 00:00:00</span>
+    <span class="author">sakochi</span>
     <a class="img-link" href="#">link</a>
   </li>
   <li class="file-list__item">
     <img class="preview-img" src="" alt="">
     <h2 class="title">sample</h2>
     <span class="created-at">2018/01/01 00:00:00</span>
+    <span class="author">sakochi</span>
     <a class="img-link" href="#">link</a>
   </li>
   <li class="file-list__item">
     <img class="preview-img" src="" alt="">
     <h2 class="title">sample</h2>
     <span class="created-at">2018/01/01 00:00:00</span>
+    <span class="author">sakochi</span>
     <a class="img-link" href="#">link</a>
   </li>
 </ul>
