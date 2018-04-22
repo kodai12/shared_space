1ee98b319a4022b374a740486aa668f0d20f44f6
sakochi_koudai <sakochi_koudai@aratana.jp>
2018-04-22 15:50:07 +0900

    add: FileList

---
+++ src/components/FileList.vue
@@ -0,0 +1,28 @@
+<template>
+<ul class="file-list">
+  <li class="file-list__item">
+    <img class="preview-img" src="" alt="">
+    <h2 class="title">sample</h2>
+    <span class="created-at">2018/01/01 00:00:00</span>
+    <a class="img-link" href="#">link</a>
+  </li>
+  <li class="file-list__item">
+    <img class="preview-img" src="" alt="">
+    <h2 class="title">sample</h2>
+    <span class="created-at">2018/01/01 00:00:00</span>
+    <a class="img-link" href="#">link</a>
+  </li>
+  <li class="file-list__item">
+    <img class="preview-img" src="" alt="">
+    <h2 class="title">sample</h2>
+    <span class="created-at">2018/01/01 00:00:00</span>
+    <a class="img-link" href="#">link</a>
+  </li>
+  <li class="file-list__item">
+    <img class="preview-img" src="" alt="">
+    <h2 class="title">sample</h2>
+    <span class="created-at">2018/01/01 00:00:00</span>
+    <a class="img-link" href="#">link</a>
+  </li>
+</ul>
+</template>
