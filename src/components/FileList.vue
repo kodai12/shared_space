bd6cd1f7cf1ab6802ff9fd2cebd1fa5048c632d7
sakochik <sakochi.kodai1209@gmail.com>
2018-04-27 13:01:06 +0900

    storybookの導入

--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -1,7 +1,10 @@
 <template>
 <ul class="file-list">
   <li class="file-list__item">
-    <img class="preview-img" src="" alt="">
+    <img
+      class="preview-img"
+      v-bind:src="require('../../static/images/arupaka.jpg')"
+      alt="">
     <h2 class="title">sample</h2>
     <span class="created-at">2018/01/01 00:00:00</span>
     <span class="author">sakochi</span>
@@ -30,3 +33,6 @@
   </li>
 </ul>
 </template>
+
+<script>
+</script>
