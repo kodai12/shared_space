ab22f9b753161c5aad2b258ce5db2763c1446327
sakochik <sakochi.kodai1209@gmail.com>
2018-04-27 15:55:07 +0900

    fix: main.js => eslint error

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import Header from './components/Header'
-import LoginForm from './components/LoginForm'
-import FileList from './components/FileList'
-import UploadButton from './components/UploadButton'
-import router from './router'
+import Vue from 'vue';
+import Header from './components/Header';
+import LoginForm from './components/LoginForm';
+import FileList from './components/FileList';
+import UploadButton from './components/UploadButton';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.component('custom-header', Header);
 Vue.component('login-form', LoginForm);
@@ -17,9 +17,9 @@ Vue.component('upload-button', UploadButton);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  delimiters: ["[[", "]]"],
+  delimiters: ['[[', ']]'],
   router,
   data: {
     message: 'Hello',
   },
-})
+});
