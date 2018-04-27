6f8f7daa8392a0c1b0df5b502600f182f381ef37
sakochik <sakochi.kodai1209@gmail.com>
2018-04-27 13:26:30 +0900

    add: storybook addons

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,8 +1,37 @@
 import { storiesOf } from '@storybook/vue'
+import { LinksTo } from '@storybook/addon-links'
+import { withKnobs, text, color } from '@storybook/addon-knobs'
+import Centered from '@storybook/addon-centered'
 
+import Header from '../components/Header.vue'
 import LoginForm from '../components/LoginForm.vue'
+import FileList from '../components/FileList.vue'
+import UploadButton from '../components/UploadButton.vue'
 
-storiesOf('LoginForm', module).add('simple', () => ({
-  components: { LoginForm },
-  template: `<LoginForm></LoginForm>`
-}));
+storiesOf('Header', module)
+  .addDecorator(Centered)
+  .add('simple', () => ({
+    components: { Header },
+    template: `<Header></Header>`,
+  }));
+
+storiesOf('LoginForm', module)
+  .addDecorator(Centered)
+  .add('simple', () => ({
+    components: { LoginForm },
+    template: `<LoginForm></LoginForm>`,
+  }));
+
+storiesOf('FileList', module)
+  .addDecorator(Centered)
+  .add('simple', () => ({
+    components: { FileList },
+    template: `<FileList></FileList>`,
+  }));
+
+storiesOf('UploadButton', module)
+  .addDecorator(Centered)
+  .add('simple', () => ({
+    components: { UploadButton },
+    template: `<UploadButton></UploadButton>`,
+  }));
