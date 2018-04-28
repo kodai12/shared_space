69eee3874d967d9e166c29e92c87ac4a4148dac5
sakochik <sakochi.kodai1209@gmail.com>
2018-04-28 12:04:59 +0900

    add: vue-routerでルーティング処理を追加

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 
 class CustomFlask(Flask):
   jinja_options = Flask.jinja_options.copy()
@@ -16,6 +16,13 @@ app = CustomFlask(__name__,
                   static_folder = './dist/static',
                   template_folder = './dist')
 
+app.config.from_pyfile('config.cfg')
+
+db = SQLAlchemy(app)
+
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/api/file_data/<int:file_id>')
+def fetchFile(file_id):
