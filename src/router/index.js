10b716af7def546d7f77e44ed2b918347fe9d214
sakochik <sakochi.kodai1209@gmail.com>
2018-04-28 12:04:59 +0900

    add: vue-routerでルーティング処理を追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import MainPage from '@/components/pages/Main';
+import LoginPage from '@/components/pages/Login';
+
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
+      component: MainPage
+    },
+    {
+      path: '/login',
+      component: LoginPage
     }
   ]
 });
