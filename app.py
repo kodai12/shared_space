70979f7bf93811418c77354ab54edc0eebb1b67f
sakochik <sakochi.kodai1209@gmail.com>
2018-04-28 18:00:05 +0900

    axiosを使ったajax処理のテスト実装

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, jsonify
+import pymysql
 
 class CustomFlask(Flask):
   jinja_options = Flask.jinja_options.copy()
@@ -12,17 +12,43 @@ class CustomFlask(Flask):
     comment_end_string='#)',
   ))
 
-app = CustomFlask(__name__,
-                  static_folder = './dist/static',
-                  template_folder = './dist')
+app = CustomFlask(
+  __name__,
+  static_folder = './dist/static',
+  template_folder = './dist'
+)
 
 app.config.from_pyfile('config.cfg')
 
-db = SQLAlchemy(app)
+def connectDb():
+  return pymysql.connect(
+    host        = 'localhost',
+    user        = 'root',
+    password    = 'kodai1209',
+    db          = 'shared_space',
+    charset     = 'utf8mb4',
+    cursorclass = pymysql.cursors.DictCursor
+  )
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/api/file_data/<int:file_id>')
-def fetchFile(file_id):
+@app.route('/api/files', methods=['GET'])
+def fetchAllFileData():
+  dbh = connectDb()
+  cursor = dbh.cursor()
+  sql = 'select f.id as id, data, '\
+    'title, u.name as author, '\
+    'f.created_at as created_at '\
+    'from files as f join users as u '\
+    'on f.uploader_id = u.id'
+  cursor.execute(sql)
+  files = cursor.fetchall()
+  cursor.close()
+  dbh.close()
+
+  return jsonify(files)
+
+if __name__ == '__main__':
+  app.run(debug=True)
