95966ccb56e0c2003a8d703c4de1c1f99a302651
sakochik <sakochi.kodai1209@gmail.com>
2018-04-30 01:14:29 +0900

    fix: style => Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,25 @@
 <template>
   <header class="header">
-    <div class="logo">
-      <img src="" alt="logo">
-    </div>
+    <h1 class="heading1">Shared Space For Aratana 18's</h1>
+    <a href="#" class="logout-link">Log Out</a>
   </header>
 </template>
 
 <style lang="scss" scoped>
 .header {
-  background-color: #fff;
+  display: flex;
+  padding: 0 30px;
+  height: 50px;
+  background-color: #e8380d;
+}
+.heading1 {
+  margin: auto 0;
+  font-size: 18px;
+  color: #fff;
+}
+.logout-link {
+  margin: auto 0 auto auto;
+  font-size: 12px;
+  color: #fff;
 }
 </style>
