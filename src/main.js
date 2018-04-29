cab1b48eb19ab9aa6f7c92959e164ae93be32390
sakochik <sakochi.kodai1209@gmail.com>
2018-04-30 01:21:51 +0900

    ElementUIの導入

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import ja from 'element-ui/lib/locale/lang/ja';
+
 import Header from './components/Header';
 import LoginForm from './components/LoginForm';
 import FileList from './components/FileList';
@@ -8,6 +13,8 @@ import UploadButton from './components/UploadButton';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(ElementUI, { ja });
 
 Vue.component('custom-header', Header);
 Vue.component('login-form', LoginForm);
