8215622ab29ccd61c4bf2eec0215541d0e6fcf56
sakochik <sakochi.kodai1209@gmail.com>
2018-04-30 01:32:15 +0900

    fix: Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,14 @@
 <template>
-  <header class="header">
+  <el-header class="header">
     <h1 class="heading1">Shared Space For Aratana 18's</h1>
     <a href="#" class="logout-link">Log Out</a>
-  </header>
+  </el-header>
 </template>
 
 <style lang="scss" scoped>
 .header {
   display: flex;
   padding: 0 30px;
-  height: 50px;
   background-color: #e8380d;
 }
 .heading1 {
