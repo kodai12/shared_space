eb657d5287080e1d4b0cc5a191a012f358889943
sakochik <sakochi.kodai1209@gmail.com>
2018-04-30 01:21:51 +0900

    ElementUIの導入

--- src/components/UploadButton.vue
+++ src/components/UploadButton.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="upload-button-wrapper">
-    <input class="upload-button" type="file">
+    <el-upload
+       class="upload-demo"
+       action="https://jsonplaceholder.typicode.com/posts/"
+       :on-preview="handlePreview"
+       :on-remove="handleRemove"
+       :file-list="fileList2"
+       list-type="picture">
+      <el-button size="small" type="primary">Click to upload</el-button>
+      <div slot="tip" class="el-upload__tip">jpg/png files with a size less than 500kb</div>
+    </el-upload>
   </div>
 </template>
 
