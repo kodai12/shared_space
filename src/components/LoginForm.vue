a787e97df28820d006a1b7c5c5e90e8b73cac5be
sakochik <sakochi.kodai1209@gmail.com>
2018-04-30 22:12:04 +0900

    fix: LoginForm

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,23 +1,81 @@
 <template>
-  <div class="login-block">
-    <form class="login__form" action="" method="POST">
-      <label class="login__label--email" for="loginEmail">E-mail: </label>
-      <input id="loginEmail" class="login__input--email" type="email">
-      <label class="login__label--password" for="loginPassword">Password: </label>
-      <input id="loginPassword" class="login__input--password" type="password">
-      <button class="login__button--submit" type="submit">Login</button>
-    </form>
-  </div>
+  <el-form
+    :model="loginForm"
+    :rules="loginRules"
+    ref="loginForm">
+    <el-form-item
+      label="email"
+      prop="email">
+      <el-input
+        v-model="loginForm.email">
+      </el-input>
+    </el-form-item>
+    <el-form-item
+      label="password"
+      prop="password">
+      <el-input
+        v-model="loginForm.password">
+      </el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('ruleForm')">Create</el-button>
+      <el-button @click="resetForm('ruleForm')">Reset</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        email: '',
+        password: ''
+      },
+      loginRules: {
+        email: [
+          {
+            required: true,
+            message: 'Please input email',
+            trigger: 'blur'
+          },{
+            type: 'email',
+            message: 'Please input a valid email address',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: 'Please input password',
+            trigger: 'change'
+          },{
+            min: 6,
+            message: 'Password is too short',
+            trigger: 'change'
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          /* eslint-disable no-console */
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
 <style lang="scss" scoped>
-.login-block {
-  width: 1000px;
-  margin: 0 auto;
-}
-.login__form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 </style>
