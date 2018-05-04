fa11e97776292dd2efbab55166db89a02c1ebc42
sakochik <sakochi.kodai1209@gmail.com>
2018-05-04 17:23:10 +0900

    add: js => Error page

---
+++ src/components/pages/Error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="error-block">
+    <h1 class="error-title">PAGE IS NOT FOUND</h1>
+    <p class="error-message">The URL may incorrect and you can return to home.</p>
+    <a href="/" class="back-home-link">
+      <el-button type="info" plain>
+        BACK TO HOME
+      </el-button>
+    </a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.error-block {
+  padding: 30px;
+  text-align: center;
+}
+.error-title {
+  margin-bottom: 10px;
+  font-size: 20px;
+}
+.error-message {
+  margin-bottom: 20px;
+  font-size: 15px;
+}
+</style>
