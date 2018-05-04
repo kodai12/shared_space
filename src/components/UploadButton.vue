1163bca75bef5e4fe9c16f72e5681f947d406b29
sakochik <sakochi.kodai1209@gmail.com>
2018-05-04 12:12:21 +0900

    add: js => ファイルアップロード処理

--- src/components/UploadButton.vue
+++ src/components/UploadButton.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="uploader-block">
+  <form
+    action="/api/file"
+    method="post"
+    enctype="multipart/form-data"
+    class="upload-form">
     <el-upload
-       class="uploader"
-       action=""
-       :on-preview="handlePreview"
-       :on-remove="handleRemove"
-       list-type="picture">
-      <el-button size="small" type="primary">Click to upload</el-button>
-      <div slot="tip" class="el-upload__tip">jpg/png files with a size less than 500kb</div>
+      class="uploader"
+      action=""
+      :http-request="uploadFile"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      list-type="picture">
+      <el-button
+        size="small"
+        type="primary">
+        Click to upload
+      </el-button>
+      <div slot="tip" class="el-upload__tip">
+        jpg/png files with a size less than 500kb
+      </div>
     </el-upload>
-  </div>
+  </form>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
@@ -29,18 +42,33 @@ export default {
   },
   methods: {
     handleRemove(file, fileList) {
-      /* eslint-disable */
+      /* eslint-disable no-console */
       console.log(file, fileList);
     },
     handlePreview(file) {
       console.log(file);
+    },
+    uploadFile() {
+      const params = new FormData();
+      const file = document.querySelector('[name="file"]');
+      params.append('file', file.files[0]);
+      console.log(file.files[0]);
+      axios.post('/api/file', params, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then((res) => {
+        console.log(res);
+      }).catch((err) => {
+        console.error(err.message);
+      });
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.uploader-block {
+.upload-form {
   margin-bottom: 30px;
 }
 .uploader {
